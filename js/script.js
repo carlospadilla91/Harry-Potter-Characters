0147c6f1384d88339de0bc4de0b4a00243df756e
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-09-23 13:03:35 -0500

    feat: added some styling and search bar working

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 "use strict"
 const characterList = document.getElementById("characterList");
 const searchBar = document.getElementById("searchBar");
+// const characters = ;
 let hpCharacters = [];
 
 searchBar.addEventListener('keyup', (e) => {
     const userInput = e.target.value.toLowerCase();
-    const filteredCharacters = hpCharacters.filter( character => {
+    const filteredCharacters = hpCharacters.filter(character => {
         return character.name.toLowerCase().includes(userInput) || character.house.toLowerCase().includes(userInput);
     })
     displayCharacters(filteredCharacters);
 })
+
+// console.log(characters);
+
+const characters = document.getElementsByClassName("character");
+for (let i = 0; i < characters.length; i += 1) {
+    characters[i].addEventListener('click', (e) => {
+        alert('clickced');
+    })
+}
+
 
 // function to load the characters from API using async and await
 const loadCharacters = async () => {
     try {
         const res = await fetch('http://hp-api.herokuapp.com/api/characters');
         hpCharacters = await res.json();
-        console.log(hpCharacters);
         displayCharacters(hpCharacters);
     } catch (err) {
         console.error(err);
@@ -26,11 +36,11 @@ const loadCharacters = async () => {
 const displayCharacters = (characters) => {
     const html = characters.map((character) => {
         return `
-        <li class="character">
-           <h2>${character.name}</h2>
-           <p>House: ${character.house}</p>
-           <img src="${character.image}">
-        </li>
+<li class="character">
+    <h2>${character.name}</h2>
+    <p>House: ${character.house}</p>
+    <img src="${character.image}">
+</li>
     `;
     })
         .join('');
@@ -38,3 +48,9 @@ const displayCharacters = (characters) => {
 }
 
 loadCharacters();
+
+
+
+
+
+
